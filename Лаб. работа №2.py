5a9c5c4775e5277d986735c7731a896d6a125aab
rasulmngoo <rasulkotov1@gmail.com>
2025-10-07 01:36:26 +0500

    Update Лаб. работа №2.py

--- Лаб. работа №2.py
+++ Лаб. работа №2.py
@@ -1,28 +1,22 @@
 a = float(input("Введите первое число: "))
 b = float(input("Введите второе число: "))
-op = input("Введите операцию (+, -, *, /, //, %, **): ")
-if op == "+":
-    print("Результат:", a + b)
-elif op == "-":
-    print("Результат:", a - b)
-elif op == "*":
-    print("Результат:", a * b)
-elif op == "/":
-    if b != 0:
-        print("Результат:", a / b)
-    else:
-        print("Ошибка: деление на ноль!")
-elif op == "//":
-    if b != 0:
-        print("Результат:", a // b)
-    else:
-        print("Ошибка: деление на ноль!")
-elif op == "%":
-    if b != 0:
-        print("Результат:", a % b)
-    else:
-        print("Ошибка: деление на ноль!")
-elif op == "**":
-    print("Результат:", a ** b)
+op = input("Введите операцию (==, !=, >, <, >=, <=, and, or): ")
+
+if op == "==":
+    print("Результат:", a == b)
+elif op == "!=":
+    print("Результат:", a != b)
+elif op == ">":
+    print("Результат:", a > b)
+elif op == "<":
+    print("Результат:", a < b)
+elif op == ">=":
+    print("Результат:", a >= b)
+elif op == "<=":
+    print("Результат:", a <= b)
+elif op == "and":
+    print("Результат:", bool(a) and bool(b))
+elif op == "or":
+    print("Результат:", bool(a) or bool(b))
 else:
     print("Неизвестная операция")
